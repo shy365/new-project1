d0dbc776e786d00b8ed9b829ec122bc0a9fcc77c
shy365 <1291547194@qq.com>
2023-06-09 09:54:52 +0800

    最终版

--- test.c
+++ test.c
@@ -1,8 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/*
+* @brrief   这是一个测试程序
+* @prame    参数个数、参数指针
+* @return   成功返回0,失败返回-1
+*/
 
-int main(int argc,char* argv)
+int main(int argc,char* argv[])
 {
 	int* p;
 	if((p = (malloc(sizeof(int)))) == NULL)
